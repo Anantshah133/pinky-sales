5100a521035780450d947bdbf3ba9e5a51eee24b
roshnirana-2 <roshnirana0369@gmail.com>
2024-01-29 19:53:15 +0530

    add product-service

--- add-productservice.php
+++ add-productservice.php
@@ -3,13 +3,13 @@ include "header.php";
 if(isset($_REQUEST['save']))
 {
  
-  $service_area_id = $_REQUEST['area_id'];
-  $pincode = $_REQUEST['pincode'];
- 
+  $product = $_REQUEST['product_id'];
+  $service = $_REQUEST['service_id'];
+  $status = $_REQUEST['default_radio'];
   try
   {
-    $stmt = $obj->con1->prepare("INSERT INTO `product_service`(`service_area_id`,`pincode`) VALUES (?,?)");
-    $stmt->bind_param("is",$service_area_id,$pincode);
+    $stmt = $obj->con1->prepare("INSERT INTO `product_service`(`pid`,`sid`,`status`) VALUES (?,?,?)");
+    $stmt->bind_param("iis",$product,$service,$status);
     $Resp=$stmt->execute();
     if(!$Resp)
     {
@@ -26,12 +26,12 @@ if(isset($_REQUEST['save']))
   if($Resp)
   {
     setcookie("msg", "data",time()+3600,"/");
-      header("location:add-areapincode.php");
+      header("location:add-productservice.php");
   }
   else
   {
     setcookie("msg", "fail",time()+3600,"/");
-      header("location:add-areapincode.php");
+      header("location:add-productservice.php");
   }
 }
 ?>
@@ -41,31 +41,52 @@ if(isset($_REQUEST['save']))
             <h5 class="text-lg font-semibold dark:text-white-light">Product-Service- Add</h5>
         </div>
         <div class="mb-5">
-            <form class="space-y-5">
+            <form class="space-y-5" method="post">
                 <div>
                     <label for="groupFname"> Product</label>
 
-                    <select class="form-select text-white-dark">
+                    <select class="form-select text-white-dark" name="product_id">
                         <option>-none-</option>
-                        <option>MIRA BHAYANDER</option>
+                        <?php 
+                        $stmt = $obj->con1->prepare("SELECT * FROM `product_category`");
+                        $stmt->execute();
+                        $Resp=$stmt->get_result();
+                        $stmt->close();
+
+                        while($result = mysqli_fetch_array($Resp)){
+                        ?>
+                    <option value="<?php echo $result['id'] ?>"><?php echo $result['name'] ?></option>
+                     <!--   <option>MIRA BHAYANDER</option>
                         <option>N H SERVICE</option>
                         <option>NO SERVICE</option>
                         <option>PALGHAR</option>
                         <option>Test Service center</option>
-                        <option>VIRAR NSP VASAI</option>
+                        <option>VIRAR NSP VASAI</option> -->
+                    <?php } ?>   
                     </select>
                 </div>
                 <div>
                     <label for="groupFname">Service</label>
 
-                    <select class="form-select text-white-dark">
+                    <select class="form-select text-white-dark" name="service_id">
                         <option>-none-</option>
-                        <option>MIRA BHAYANDER</option>
+                        <?php 
+                        $stmt = $obj->con1->prepare("SELECT * FROM `service_type`");
+                        $stmt->execute();
+                        $Resp=$stmt->get_result();
+                        $stmt->close();
+
+                        while($result = mysqli_fetch_array($Resp)){
+                        ?>
+                    <option value="<?php echo $result['id'] ?>"><?php echo $result['name'] ?></option>
+                    
+                      <!--  <option>MIRA BHAYANDER</option>
                         <option>N H SERVICE</option>
                         <option>NO SERVICE</option>
                         <option>PALGHAR</option>
                         <option>Test Service center</option>
-                        <option>VIRAR NSP VASAI</option>
+                        <option>VIRAR NSP VASAI</option>-->
+                        <?php } ?>    
                     </select>
                 </div>
                 <div>
@@ -81,7 +102,7 @@ if(isset($_REQUEST['save']))
                 </div>
                     
                     <div class="relative inline-flex align-middle gap-3 mt-4">
-                        <button type="button" class="btn btn-primary">Save </button>
+                        <button type="submit" name="save" id="save" class="btn btn-primary">Save </button>
                         <button type="button" class="btn  btn-warning ">Close</button>
                     </div>
                 </div>
