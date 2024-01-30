75cff27782ca49e3e548783fa1056deb9d2bc0ac
roshnirana-2 <roshnirana0369@gmail.com>
2024-01-30 16:33:35 +0530

    add city

--- add-state.php
+++ add-state.php
@@ -37,7 +37,7 @@ if(isset($_REQUEST['save']))
 <div class='p-6' x-data='exportTable'>
     <div class="panel mt-6">
         <div class='flex items-center justify-between mb-3'>
-            <h5 class="text-lg font-semibold dark:text-white-light">Service Area- Add</h5>
+            <h5 class="text-lg font-semibold dark:text-white-light">State- Add</h5>
         </div>
         <div class="mb-5">
             <form class="space-y-5" method="post">
