423401e1ffa3c7e9f21903ce725d860525f38336
Neha <nehasurati1605@gmail.com>
2024-02-02 15:11:00 +0530

    delete query added in privcy policy

--- privacy_policy.php
+++ privacy_policy.php
@@ -1,5 +1,33 @@
 <?php
 include "header.php";
+if(isset($_REQUEST["flg"]) && $_REQUEST["flg"]=="del")
+{
+  try
+  {
+   
+    $stmt_del = $obj->con1->prepare("delete from privacy_policy where id='".$_REQUEST["id"]."'");
+    $Resp=$stmt_del->execute();
+    if(!$Resp)
+    {
+      if(strtok($obj->con1-> error,  ':')=="Cannot delete or update a parent row")
+      {
+        throw new Exception("City is already in use!");
+      }
+    }
+    $stmt_del->close();
+  }
+  catch(\Exception  $e) {
+    setcookie("sql_error", urlencode($e->getMessage()),time()+3600,"/");
+  }
+
+  if($Resp)
+  {
+    setcookie("msg", "data_del",time()+3600,"/");
+    // echo "this data is deleted";
+    // echo "<script type='text/javascript'>coloredToast('success')</script>";
+  }
+    header("location:privacy_policy.php");
+}
 ?>
 
 <div class='p-6' x-data='exportTable'>
@@ -27,7 +55,59 @@ include "header.php";
 <!-- script -->
 
 <script>
-function getActions() {
+
+function createCookie(name, value, days) {
+    var expires;
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toGMTString();
+    } else {
+        expires = "";
+    }
+    document.cookie = (name) + "=" + String(value) + expires + ";path=/ ";
+
+}
+
+function readCookie(name) {
+    var nameEQ = (name) + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) === 0) return (c.substring(nameEQ.length, c.length));
+    }
+    return null;
+}
+
+function eraseCookie(name) {
+    createCookie(name, "", -1);
+}
+coloredToast = (color) => {
+    const toast = window.Swal.mixin({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 3000,
+        showCloseButton: true,
+        customClass: {
+            popup: `color-${color}`
+        },
+    });
+    toast.fire({
+        title: 'Record Deleted Successfully.',
+        onClose: () => {
+            document.cookie = "msg=data_del; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+        }
+    });
+};
+
+if (readCookie("msg") == "data_del") {
+    coloredToast("success");
+    eraseCookie("msg")
+}
+
+function getActions(id) {
     return `<ul class="flex items-center  gap-4">
         <li>
             <a href="javascript:;" class='text-xl' x-tooltip="View">
@@ -40,7 +120,7 @@ function getActions() {
             </a>
         </li>
         <li>
-            <a href="javascript:;" class='text-xl' x-tooltip="Delete">
+            <a href="javascript:;" class='text-xl' x-tooltip="Delete" @click="showAlert(` + id + `)">
                 <i class="ri-delete-bin-line text-danger"></i>
             </a>
         </li>
@@ -71,7 +151,8 @@ document.addEventListener('alpine:init', () => {
                                          
                       ?>
                    
-                   ['<?php echo $id ?>','<?php echo $row["detail"] ?>', '<?php echo $row["type"] ?>',' <?php echo $row["date_time"] ?>', getActions()],
+                   ['<?php echo $id ?>','<?php echo $row["detail"] ?>', '<?php echo $row["type"] ?>',' <?php echo $row["date_time"] ?>', getActions((
+                                '<?php echo $row['id'] ?>'))],
                       <?php 
                       $id++;	
                           }
@@ -137,6 +218,24 @@ document.addEventListener('alpine:init', () => {
         },
     }));
 })
+
+async function showAlert(id) {
+    new window.Swal({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        showCancelButton: true,
+        confirmButtonText: 'Delete',
+        padding: '2em',
+    }).then((result) => {
+        console.log(result)
+        if (result.isConfirmed) {
+            var loc = "privacy_policy.php?flg=del&id=" + id;
+            window.location = loc;
+
+            // coloredToast('success')
+        }
+    });
+}
 </script>
 
 <?php
