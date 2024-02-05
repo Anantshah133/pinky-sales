b0e56308354669d5850a047fec857188a33e5281
Anantshah133 <anantshah130305@gmail.com>
2024-02-05 13:35:42 +0530

    View completed in area pincode

--- add_area_pincode.php
+++ add_area_pincode.php
@@ -1,16 +1,18 @@
 <?php
 include "header.php";
 if (isset($_REQUEST["viewId"])) {
-    $viewId = $_REQUEST["viewId"];
-    $stmt = $obj->con1->prepare("SELECT * FROM `area_pincode` where srno=?");
+    $mode = 'view';
+    $viewId = $_REQUEST["viewId"]; // currently 15
+    $stmt = $obj->con1->prepare("SELECT a1.*, s1.name AS state, c1.ctnm AS city FROM `area_pincode` a1, `state` s1, `city` c1 WHERE a1.id=? AND a1.state_id=s1.id AND a1.city_id=c1.srno");
     $stmt->bind_param("i", $viewId);
     $stmt->execute();
     $Resp = $stmt->get_result();
     $data = $Resp->fetch_assoc();
     $stmt->close();
 }
+
 if (isset($_REQUEST["save"])) {
-    $state_name = $_REQUEST["State_id"];
+    $state_name = $_REQUEST["state_id"];
     $city_name = $_REQUEST["area_id"];
     $pincode = $_REQUEST["pincode"];
 
@@ -48,48 +50,35 @@ if (isset($_REQUEST["save"])) {
         <div class="mb-5">
             <form class="space-y-5" method="post">
                 <div>
-                    
                     <label for="groupFname"> State Name</label>
-                    <select class="form-select text-white-dark" onchange="loadCities(this.value)" name="State_id" required>
-                        <option value="">Choose State</option>
-                    <?php
-                    $stmt = $obj->con1->prepare("SELECT * FROM `state`");
-                    $stmt->execute();
-                    $Resp = $stmt->get_result();
-                    $stmt->close();
-
-                    while ($result = mysqli_fetch_array($Resp)) { ?>
-                        <option value="<?php echo $result[
-                            "id"
-                        ]; ?>"><?php echo $result["name"]; ?></option>
-                    <?php }
-                    ?>
+                    <select class="form-select text-white-dark" onchange="loadCities(this.value)" name="state_id" required <?php echo (isset($mode) == 'view') ? 'disabled' : '' ?>>
+                        <option value=""><?php echo isset($mode) ? $data['state'] : 'Choose State' ?></option>
+                        <?php
+                            $stmt = $obj->con1->prepare("SELECT * FROM `state`");
+                            $stmt->execute();
+                            $Resp = $stmt->get_result();
+                            $stmt->close();
+                            while ($result = mysqli_fetch_array($Resp)) { 
+                        ?>
+                            <option value="<?php echo $result["id"]; ?>"><?php echo $result["name"]; ?></option>
+                        <?php 
+                            }
+                        ?>
                     </select>
                 </div>
                 <div>
                     
                     <label for="groupFname">City Name</label>
-
-                    <select class="form-select text-white-dark" name="area_id" id="area_id" required>
-                        <option value="">Choose City</option>
-                    <?php
-                    $stmt = $obj->con1->prepare("SELECT * FROM `city`");
-                    $stmt->execute();
-                    $Resp = $stmt->get_result();
-                    $stmt->close();
-
-                    while ($result = mysqli_fetch_array($Resp)) { ?>
-                        <option value="<?php echo $result[
-                            "srno"
-                        ]; ?>"><?php echo $result["ctnm"]; ?></option>
-                    <?php }
-                    ?>
+                    <select class="form-select text-white-dark" name="area_id" id="area_id" 
+                    required <?php echo (isset($mode) == 'view') ? 'disabled' : '' ?>>
+                        <option value=""><?php echo isset($mode) ? $data['city'] : 'Choose City' ?></option>
                     </select>
                 </div>
                 <div>
                     <label for="groupFname"> Pincode </label>
-                    <input id="groupFname" name="pincode" type="text" class="form-input" />
-                    <div class="relative inline-flex align-middle gap-3 mt-4">
+                    <input id="groupFname" name="pincode" type="text" class="form-input" 
+                    <?php echo (isset($mode) == 'view') ? 'readonly' : '' ?> value="<?php echo isset($mode) ? data['pincode'] : '' ?>" />
+                    <div class="relative inline-flex align-middle gap-3 mt-4 <?php echo isset($mode) == 'view' ? 'hidden' : '' ?>">
                         <button type="submit" name="save" id="save" class="btn btn-success">Save </button>
                         <button type="button" class="btn btn-danger" onclick="window.location='area_pincode.php'">Close</button>
                     </div>
