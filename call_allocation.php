8306ddf47de177ccb810aa05c40a7440fe63ddb8
Neha <nehasurati1605@gmail.com>
2024-02-07 19:48:50 +0530

    Delete query added  in call allocation

--- call_allocation.php
+++ call_allocation.php
@@ -1,5 +1,27 @@
 <?php
 include "header.php";
+
+if (isset($_REQUEST["flg"]) && $_REQUEST["flg"] == "del") {
+    try {
+        $stmt_del = $obj->con1->prepare(
+            "delete from `call_allocation` where id='" . $_REQUEST["id"] . "'"
+        );
+        $Resp = $stmt_del->execute();
+        if (!$Resp) {
+            if (strtok($obj->con1->error, ":") == "Cannot delete or update a parent row") {
+                throw new Exception("City is already in use!");
+            }
+        }
+        $stmt_del->close();
+    } catch (\Exception $e) {
+        setcookie("sql_error", urlencode($e->getMessage()), time() + 3600, "/");
+    }
+
+    if ($Resp) {
+        setcookie("msg", "data_del", time() + 3600, "/");
+    }
+    header("location:call_allocation.php");
+}
 ?>
 
 <div class='p-6' x-data='callAllocationTable'>
@@ -31,17 +53,17 @@ checkCookies();
 function getActions(id) {
     return `<ul class="flex items-center justify-center gap-4">
         <li>
-            <a href="add_call_allocation.php?viewId=${id}" class='text-xl' x-tooltip="View">
+            <a href="javascript:;" class='text-xl' x-tooltip="View">
                 <i class="ri-eye-line text-primary"></i>
             </a>
         </li>
         <li>
-            <a href="add_call_allocation.php?editId=${id}" class='text-xl' x-tooltip="Edit">
+            <a href="javascript:;" class='text-xl' x-tooltip="Edit">
                 <i class="ri-pencil-line text text-success"></i>
             </a>
         </li>
         <li>
-            <a href="javascript:;" class='text-xl' x-tooltip="Delete">
+            <a href="javascript:;" class='text-xl' x-tooltip="Delete"  @click="showAlert(${id})">
                 <i class="ri-delete-bin-line text-danger"></i>
             </a>
         </li>
@@ -57,13 +79,13 @@ document.addEventListener('alpine:init', () => {
             this.datatable = new simpleDatatables.DataTable('#call-table', {
                 data: {
                     headings: ['Sr.No.', 'Complaint No.', 'Service Center', 'Technician',
-                        'Allocation Date', 'Allocation Time', 'Status',
+                        'Allocation Date', 'Status', 'Allocation Time',
                         'Reason', 'Customer Name', 'Customer Contact',
                         'Product Category', 'Action'
                     ],
                     data: [
                         <?php 
-                            $stmt = $obj->con1->prepare("select tbl.*,t1.name as technician_name from (select c1.*,s1.name as service_center_name,p1.name as product_category_name,CONCAT(c2.fname,' ',c2.lname) as customer_name,c2.contact as customer_contact FROM call_allocation c1,customer_reg c2,service_center s1,product_category p1 where c1.complaint_no=c2.complaint_no and c1.service_center_id=s1.id and c2.product_category=p1.id ) as tbl LEFT JOIN technician t1 on tbl.technician=t1.id order by tbl.id desc");
+                            $stmt = $obj->con1->prepare("SELECT * FROM call_allocation");
                             $stmt->execute();
                             $Resp=$stmt->get_result();
                             $i = 1;
@@ -71,16 +93,15 @@ document.addEventListener('alpine:init', () => {
                         ?>
                             [
                                 '<?php echo $i ?>', '<?php echo $row['complaint_no'] ?>',
-                                '<?php echo $row['service_center_name'] ?>',
-                                '<?php echo $row['technician_name'] ?>',
+                                '<?php echo $row['service_center_id'] ?>',
+                                '<?php echo $row['technician'] ?>',
                                 '<?php echo $row['allocation_date'] ?>',
-                                '<?php echo $row['allocation_time'] ?>',
                                 '<?php echo $row['status'] ?>',
+                                '<?php echo $row['allocation_time'] ?>',
                                 '<?php echo $row['reason'] ?>',
-                                '<?php echo $row['customer_name'] ?>',
-                                '<?php echo $row['customer_contact'] ?>',
-                                '<?php echo $row['product_category_name'] ?>',
-                                getActions(<?php echo $row['id'] ?>)
+                                '<?php echo $row['complaint_no'] ?>',
+                                '<?php echo $row['complaint_no'] ?>',
+                                '<?php echo $row['complaint_no'] ?>', getActions()
                             ],
                         <?php
                             $i++;
@@ -140,6 +161,20 @@ document.addEventListener('alpine:init', () => {
         },
     }));
 })
+async function showAlert(id) {
+    new window.Swal({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        showCancelButton: true,
+        confirmButtonText: 'Delete',
+        padding: '2em',
+    }).then((result) => {
+        if (result.isConfirmed) {
+            var loc = "call_allocation.php?flg=del&id=" + id;
+            window.location = loc;
+        }
+    });
+}
 </script>
 
 <?php
