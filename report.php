039ef6febd94f67bd3a21a89ae0548abbd9d2bba
Neha <nehasurati1605@gmail.com>
2024-02-09 15:04:49 +0530

    report page ui change

--- report.php
+++ report.php
@@ -10,20 +10,14 @@ include "header.php";
         </div>
 
         <form x-data="form" class="space-y-5">
-            <div class="flex">
-                <div>
-                    <label for="range-calendar">Date Range</label>
-                    <input id="range-calendar" x-model="date3" class="form-input" />
-                </div>
-            </div>
 
             <div class="flex">
-                <div class="w-6/12 px-2">
+                <div class="w-6/12 px-4">
                     <label for="groupFname"> Service Center</label>
 
-                    <select class="form-select text-white-dark" name="service_center" required >
-                    <option value="">Choose...</option>
-                    <?php
+                    <select class="form-select text-white-dark" name="service_center" required>
+                        <option value="">Choose...</option>
+                        <?php
                             $stmt = $obj->con1->prepare(
                                 "SELECT * FROM `service_center` WHERE status='enable'"
                             );
@@ -33,19 +27,19 @@ include "header.php";
 
                             while ($result = mysqli_fetch_assoc($Res)) { 
                         ?>
-                    <option value="<?php echo $result["id"]; ?>"><?php echo $result["name"]; ?></option>
-                    <?php 
+                        <option value="<?php echo $result["id"]; ?>"><?php echo $result["name"]; ?></option>
+                        <?php 
                             } 
                         ?>
-                </select>
+                    </select>
                 </div>
 
-                <div class="w-6/12 px-2">
+                <div class="w-6/12 px-4 ">
                     <label for="groupFname"> Technician</label>
 
-                    <select class="form-select text-white-dark" name="" required >
-                    <option value="">Choose...</option>
-                    <?php
+                    <select class="form-select text-white-dark" name="" required>
+                        <option value="">Choose...</option>
+                        <?php
                             $stmt = $obj->con1->prepare(
                                 "SELECT * FROM `technician` WHERE status='enable'"
                             );
@@ -55,50 +49,58 @@ include "header.php";
 
                             while ($result = mysqli_fetch_assoc($Res)) { 
                         ?>
-                    <option value="<?php echo $result["id"]; ?>"><?php echo $result["name"]; ?></option>
-                    <?php 
+                        <option value="<?php echo $result["id"]; ?>"><?php echo $result["name"]; ?></option>
+                        <?php 
                             } 
                         ?>
-                </select>
-                </div>
-            </div>
-
-            <div>
-                <label for="gridStatus">Status</label>
-                <div class="flex gap-5 items-center">
-                    <div class="">
-                        <label class="">
-                            <input type="radio" name="default_radio" class="form-radio" checked />
-                            <span>Pending</span>
-                        </label>
-                    </div>
-                    <div class="">
-                        <label class="">
-                            <input type="radio" name="default_radio" class="form-radio text-primary" />
-                            <span>Allocated</span>
-                        </label>
-                    </div>
-                    <div class="">
-                        <label class="">
-                            <input type="radio" name="default_radio" class="form-radio text-primary" />
-                            <span>Closed</span>
-                        </label>
-                    </div>
-                    <div class="">
-                        <label class="">
-                            <input type="radio" name="default_radio" class="form-radio text-primary" />
-                            <span>Cancelled</span>
-                        </label>
-                    </div>
+                    </select>
                 </div>
             </div>
             <div class="flex">
-                <div class="w-6/12 px-2">
+            <div class="w-6/12 px-4">
+                    <label for="range-calendar">Date Range</label>
+                    <input id="range-calendar" x-model="date3" class="form-input" />
+                </div>
+
+                <div class="w-6/12  px-4 ">
+                    <label for="gridStatus">Status</label>
+                    <div class="flex gap-5 items-center mt-3">
+                        <div class="">
+                            <label class="">
+                                <input type="radio" name="default_radio" class="form-radio" checked />
+                                <span>Pending</span>
+                            </label>
+                        </div>
+                        <div class="">
+                            <label class="">
+                                <input type="radio" name="default_radio" class="form-radio text-primary" />
+                                <span>Allocated</span>
+                            </label>
+                        </div>
+                        <div class="">
+                            <label class="">
+                                <input type="radio" name="default_radio" class="form-radio text-primary" />
+                                <span>Closed</span>
+                            </label>
+                        </div>
+                        <div class="">
+                            <label class="">
+                                <input type="radio" name="default_radio" class="form-radio text-primary" />
+                                <span>Cancelled</span>
+                            </label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="flex">
+                <div class="w-6/12 px-4">
                     <label for="groupFname"> Complaint No</label>
 
-                    <select class="form-select text-white-dark" name="" required >
-                    <option value="">Choose...</option>
-                    <?php
+                    <select class="form-select text-white-dark" name="" required>
+                        <option value="">Choose...</option>
+                        <?php
                             $stmt = $obj->con1->prepare(
                                 "SELECT * FROM `complaint` "
                             );
@@ -108,19 +110,19 @@ include "header.php";
 
                             while ($result = mysqli_fetch_assoc($Res)) { 
                         ?>
-                    <option value="<?php echo $result["id"]; ?>"><?php echo $result["name"]; ?></option>
-                    <?php 
+                        <option value="<?php echo $result["id"]; ?>"><?php echo $result["name"]; ?></option>
+                        <?php 
                             } 
                         ?>
-                </select>
+                    </select>
                 </div>
 
-                <div class="w-6/12 px-2">
+                <div class="w-6/12 px-4 ">
                     <label for="groupFname"> Contact No</label>
 
-                    <select class="form-select text-white-dark" name="" required >
-                    <option value="">Choose...</option>
-                    <?php
+                    <select class="form-select text-white-dark" name="" required>
+                        <option value="">Choose...</option>
+                        <?php
                             $stmt = $obj->con1->prepare(
                                 "SELECT * FROM `contact` "
                             );
@@ -130,14 +132,16 @@ include "header.php";
 
                             while ($result = mysqli_fetch_assoc($Res)) { 
                         ?>
-                    <option value="<?php echo $result["id"]; ?>"><?php echo $result["name"]; ?></option>
-                    <?php 
+                        <option value="<?php echo $result["id"]; ?>"><?php echo $result["name"]; ?></option>
+                        <?php 
                             } 
                         ?>
-                </select>
+                    </select>
                 </div>
             </div>
-            <button type="button" class="btn btn-success">Submit</button>
+            <div class="px-4">
+            <button type="button" class="btn btn-success ">Submit</button>
+            </div>
         </form>
     </div>
 </div>
@@ -145,25 +149,25 @@ include "header.php";
 <!-- script -->
 <script src="assets/js/flatpickr.js"></script>
 <script>
-    document.addEventListener("alpine:init", () => {
-        let todayDate = new Date();
-        let formattedToday = todayDate.toLocaleDateString('en-GB', {
-            day: '2-digit',
-            month: '2-digit',
-            year: 'numeric',
-        }).split('/').join('-');
+document.addEventListener("alpine:init", () => {
+    let todayDate = new Date();
+    let formattedToday = todayDate.toLocaleDateString('en-GB', {
+        day: '2-digit',
+        month: '2-digit',
+        year: 'numeric',
+    }).split('/').join('-');
 
-        Alpine.data("form", () => ({
-            date3: `${formattedToday} to ${formattedToday}`,
-            init() {
-                flatpickr(document.getElementById('range-calendar'), {
-                    defaultDate: this.date3,
-                    dateFormat: 'd-m-Y',
-                    mode: 'range',
-                    minDate: formattedToday,
-                })
-            }
-        }));
+    Alpine.data("form", () => ({
+        date3: `${formattedToday} to ${formattedToday}`,
+        init() {
+            flatpickr(document.getElementById('range-calendar'), {
+                defaultDate: this.date3,
+                dateFormat: 'd-m-Y',
+                mode: 'range',
+                minDate: formattedToday,
+            })
+        }
+    }));
 
 });
 </script>
