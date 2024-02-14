05db7b25c761bb0eb2ac946f9795fe331d9c13c7
Neha <nehasurati1605@gmail.com>
2024-02-14 16:38:03 +0530

    view,edit cookie add in service center service type amd technician

--- technician.php
+++ technician.php
@@ -1,5 +1,7 @@
 <?php
 include "header.php";
+setcookie("editId", "", time() - 3600);
+setcookie("viewId", "", time() - 3600);
 
 if (isset($_REQUEST["flg"]) && $_REQUEST["flg"] == "del") {
     try {
@@ -53,12 +55,12 @@ checkCookies();
 function getActions(id,name) {
     return `<ul class="flex items-center justify-center gap-4">
         <li>
-            <a href="add_technician.php?viewId=${id}" class='text-xl' x-tooltip="View">
+            <a href="javascript:viewRecord(${id}, 'add_technician.php')" class='text-xl' x-tooltip="View">
                 <i class="ri-eye-line text-primary"></i>
             </a>
         </li>
         <li>
-            <a href="add_technician.php?editId=${id}" class='text-xl' x-tooltip="Edit">
+            <a href="javascript:updateRecord(${id}, 'add_technician.php');" class='text-xl' x-tooltip="Edit">
                 <i class="ri-pencil-line text text-success"></i>
             </a>
         </li>
