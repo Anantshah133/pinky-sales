920b2a47adc8b035367a202f76030646e68c9b46
roshnirana-2 <roshnirana0369@gmail.com>
2024-04-12 19:18:54 +0530

    add ajax in this pages

--- add_product_service.php
+++ add_product_service.php
@@ -81,7 +81,7 @@ if (isset($_REQUEST["save"])) {
             <form class="space-y-5" method="post" id="mainForm">
                 <div>
                     <label for="groupFname">Product</label>
-                    <select class="form-select text-white-dark" name="product_id" required
+                    <select class="form-select text-white-dark" name="product_id" id="product_id" required onchange="<?php echo isset($mode) ? '':'get_service_list(this.value)' ?>"
                         <?php echo isset($mode) && $mode == 'view' ? 'disabled' : '' ?>>
                         <option value="">Choose Product</option>
                         <?php
@@ -105,18 +105,19 @@ if (isset($_REQUEST["save"])) {
                 </div>
                 <div>
                     <label for="groupFname">Service</label>
-                    <select class="form-select text-white-dark" name="service_id" required
+                    <select class="form-select text-white-dark" name="service_id" id="service_id" required
                         <?php echo isset($mode) && $mode == 'view' ?'disabled' :''?>>
                         <option value="">Choose Service</option>
                         <?php
-                            $stmt = $obj->con1->prepare(
-                                "SELECT * FROM `service_type`"
-                            );
-                            $stmt->execute();
-                            $Resp = $stmt->get_result();
-                            $stmt->close();
+                            if(isset($mode)){
+                                $stmt = $obj->con1->prepare(
+                                    "SELECT * FROM `service_type`"
+                                );
+                                $stmt->execute();
+                                $Resp = $stmt->get_result();
+                                $stmt->close();
 
-                            while ($result = mysqli_fetch_array($Resp)) { 
+                                while ($result = mysqli_fetch_array($Resp)) { 
                         ?>
                             <option value="<?php echo $result["id"]; ?>"
                             <?php echo isset($mode) && $data["sid"] == $result["id"] ? "selected" : ""; ?>
@@ -124,10 +125,11 @@ if (isset($_REQUEST["save"])) {
                                 <?php echo $result["name"]; ?>
                             </option>
                         <?php 
-                            }
+                            } }
                         ?>
                     </select>
                 </div>
+                <p class="mt-3 text-danger text-base font-bold" id="demo"></p>
                 <div>
                     <label for="gridStatus">Status</label>
                     <label class="inline-flex mr-3">
@@ -148,7 +150,7 @@ if (isset($_REQUEST["save"])) {
                 <div class="relative inline-flex align-middle gap-3 mt-4">
                     <!-- Save/Update button -->
                     <button type="submit" name="<?php echo isset($mode) && $mode == 'edit' ? 'update' : 'save' ?>"
-                    id="save" class="btn btn-success" onclick="return validateAndDisable()" <?php echo isset($mode) && $mode == 'view' ? 'style="display:none;"' : '' ?>>
+                    id="save" class="btn btn-success" onclick="return localValidate()" <?php echo isset($mode) && $mode == 'view' ? 'style="display:none;"' : '' ?>>
                         <?php echo isset($mode) && $mode == 'edit' ? 'Update' : 'Save' ?>
                     </button>
                     <!-- Close button -->
@@ -160,6 +162,68 @@ if (isset($_REQUEST["save"])) {
         </div>
     </div>
 </div>
+
+<script>
+    
+    function localValidate(){
+        let form = document.getElementById('mainForm');
+        let product_id = document.getElementById('product_id');
+        let service_id = document.getElementById('service_id');
+        let submitButton = document.getElementById('save');
+        <?php if (isset($mode)) { ?>
+            if(form.checkValidity() && check_product_service(product_id.value,service_id.value,'<?php echo $data['srno'] ?>')){
+                console.log("if")
+                setTimeout(() => {
+                    submitButton.disabled = true;
+                }, 0);
+                return true;
+            }
+        <?php } else { ?>
+                console.log("else")
+                if(form.checkValidity()){
+                setTimeout(() => {
+                    submitButton.disabled = true;
+                }, 0);
+                return true;
+        }
+        <?php } ?>
+    }
+
+    function get_service_list(product_id){
+        const obj = new XMLHttpRequest();
+        obj.open("GET",`./ajax/get_service_list.php?product_id=${product_id}`, false);
+        obj.send();
+
+        if(obj.status == 200){
+            let x = obj.responseText;
+            console.log(x);
+            document.getElementById("service_id").innerHTML = "";
+            document.getElementById("service_id").innerHTML = x;
+        }
+    }
+
+    function check_product_service(product_id,service_id,pid){
+        console.log("yes");
+        const obj = new XMLHttpRequest();
+        obj.open("GET",`./ajax/check_product_service.php?product_id=${product_id}&service_id=${service_id}&pid=${pid}`, false);
+        obj.send();
+        
+        if(obj.status == 200){
+            let x = obj.responseText;
+            console.log(x);
+            document.getElementById('service_id').value = "";
+            if (x >= 1) {
+                document.getElementById("demo").innerHTML = "Sorry this Product Service Already Exist!";
+                return false;
+            }
+            else {
+                document.getElementById("demo").innerHTML = "";
+                return true;
+            }
+        }
+    }
+
+</script>
 
 <?php include "footer.php";
 ?>
